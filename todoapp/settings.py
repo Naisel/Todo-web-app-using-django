c8d63caf11b28d6a251043f694b6c47b43456a63
Naisel <naisel329@gmail.com>
2022-07-13 22:52:23 +0530

    Update settings.py

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import os
 import dj_database_url
 prod_db = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -86,6 +86,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+DATABASES['default'].update(prod_db)
 
 
 # Password validation
